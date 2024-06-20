25adece3c54fe96c65cf2e75ded164cf05628eaf
Jasonlinn <jasonlinn@example.com>
2024-06-20 23:53:49 +0800

    use flex message carousel

--- src/index.js
+++ src/index.js
@@ -12,156 +12,269 @@ const bot = new linebot({
 // 程式碼都寫在下面這個區塊內
 bot.on('message', async event => {
   let msg = {
-    "type": "bubble",
-    "hero": {
-      "type": "image",
-      "url": "https://developers-resource.landpress.line.me/fx/img/01_1_cafe.png",
-      "size": "full",
-      "aspectRatio": "20:13",
-      "aspectMode": "cover",
-      "action": {
-        "type": "uri",
-        "uri": "https://line.me/"
+    "type": "carousel",
+    "contents": [
+      {
+        "type": "bubble",
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "image",
+              "url": "https://developers-resource.landpress.line.me/fx/clip/clip1.jpg",
+              "size": "full",
+              "aspectMode": "cover",
+              "aspectRatio": "2:3",
+              "gravity": "top"
+            },
+            {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "box",
+                  "layout": "vertical",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "Brown's T-shirts",
+                      "size": "xl",
+                      "color": "#ffffff",
+                      "weight": "bold"
+                    }
+                  ]
+                },
+                {
+                  "type": "box",
+                  "layout": "baseline",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "¥35,800",
+                      "color": "#ebebeb",
+                      "size": "sm",
+                      "flex": 0
+                    },
+                    {
+                      "type": "text",
+                      "text": "¥75,000",
+                      "color": "#ffffffcc",
+                      "decoration": "line-through",
+                      "gravity": "bottom",
+                      "flex": 0,
+                      "size": "sm"
+                    }
+                  ],
+                  "spacing": "lg"
+                },
+                {
+                  "type": "box",
+                  "layout": "vertical",
+                  "contents": [
+                    {
+                      "type": "filler"
+                    },
+                    {
+                      "type": "box",
+                      "layout": "baseline",
+                      "contents": [
+                        {
+                          "type": "filler"
+                        },
+                        {
+                          "type": "icon",
+                          "url": "https://developers-resource.landpress.line.me/fx/clip/clip14.png"
+                        },
+                        {
+                          "type": "text",
+                          "text": "Add to cart",
+                          "color": "#ffffff",
+                          "flex": 0,
+                          "offsetTop": "-2px"
+                        },
+                        {
+                          "type": "filler"
+                        }
+                      ],
+                      "spacing": "sm"
+                    },
+                    {
+                      "type": "filler"
+                    }
+                  ],
+                  "borderWidth": "1px",
+                  "cornerRadius": "4px",
+                  "spacing": "sm",
+                  "borderColor": "#ffffff",
+                  "margin": "xxl",
+                  "height": "40px"
+                }
+              ],
+              "position": "absolute",
+              "offsetBottom": "0px",
+              "offsetStart": "0px",
+              "offsetEnd": "0px",
+              "backgroundColor": "#03303Acc",
+              "paddingAll": "20px",
+              "paddingTop": "18px"
+            },
+            {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "SALE",
+                  "color": "#ffffff",
+                  "align": "center",
+                  "size": "xs",
+                  "offsetTop": "3px"
+                }
+              ],
+              "position": "absolute",
+              "cornerRadius": "20px",
+              "offsetTop": "18px",
+              "backgroundColor": "#ff334b",
+              "offsetStart": "18px",
+              "height": "25px",
+              "width": "53px"
+            }
+          ],
+          "paddingAll": "0px"
+        }
+      },
+      {
+        "type": "bubble",
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "image",
+              "url": "https://developers-resource.landpress.line.me/fx/clip/clip2.jpg",
+              "size": "full",
+              "aspectMode": "cover",
+              "aspectRatio": "2:3",
+              "gravity": "top"
+            },
+            {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "box",
+                  "layout": "vertical",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "Cony's T-shirts",
+                      "size": "xl",
+                      "color": "#ffffff",
+                      "weight": "bold"
+                    }
+                  ]
+                },
+                {
+                  "type": "box",
+                  "layout": "baseline",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "¥35,800",
+                      "color": "#ebebeb",
+                      "size": "sm",
+                      "flex": 0
+                    },
+                    {
+                      "type": "text",
+                      "text": "¥75,000",
+                      "color": "#ffffffcc",
+                      "decoration": "line-through",
+                      "gravity": "bottom",
+                      "flex": 0,
+                      "size": "sm"
+                    }
+                  ],
+                  "spacing": "lg"
+                },
+                {
+                  "type": "box",
+                  "layout": "vertical",
+                  "contents": [
+                    {
+                      "type": "filler"
+                    },
+                    {
+                      "type": "box",
+                      "layout": "baseline",
+                      "contents": [
+                        {
+                          "type": "filler"
+                        },
+                        {
+                          "type": "icon",
+                          "url": "https://developers-resource.landpress.line.me/fx/clip/clip14.png"
+                        },
+                        {
+                          "type": "text",
+                          "text": "Add to cart",
+                          "color": "#ffffff",
+                          "flex": 0,
+                          "offsetTop": "-2px"
+                        },
+                        {
+                          "type": "filler"
+                        }
+                      ],
+                      "spacing": "sm"
+                    },
+                    {
+                      "type": "filler"
+                    }
+                  ],
+                  "borderWidth": "1px",
+                  "cornerRadius": "4px",
+                  "spacing": "sm",
+                  "borderColor": "#ffffff",
+                  "margin": "xxl",
+                  "height": "40px"
+                }
+              ],
+              "position": "absolute",
+              "offsetBottom": "0px",
+              "offsetStart": "0px",
+              "offsetEnd": "0px",
+              "backgroundColor": "#9C8E7Ecc",
+              "paddingAll": "20px",
+              "paddingTop": "18px"
+            },
+            {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "SALE",
+                  "color": "#ffffff",
+                  "align": "center",
+                  "size": "xs",
+                  "offsetTop": "3px"
+                }
+              ],
+              "position": "absolute",
+              "cornerRadius": "20px",
+              "offsetTop": "18px",
+              "backgroundColor": "#ff334b",
+              "offsetStart": "18px",
+              "height": "25px",
+              "width": "53px"
+            }
+          ],
+          "paddingAll": "0px"
+        }
       }
-    },
-    "body": {
-      "type": "box",
-      "layout": "vertical",
-      "contents": [
-        {
-          "type": "text",
-          "text": "Brown Cafe",
-          "weight": "bold",
-          "size": "xl"
-        },
-        {
-          "type": "box",
-          "layout": "baseline",
-          "margin": "md",
-          "contents": [
-            {
-              "type": "icon",
-              "size": "sm",
-              "url": "https://developers-resource.landpress.line.me/fx/img/review_gold_star_28.png"
-            },
-            {
-              "type": "icon",
-              "size": "sm",
-              "url": "https://developers-resource.landpress.line.me/fx/img/review_gold_star_28.png"
-            },
-            {
-              "type": "icon",
-              "size": "sm",
-              "url": "https://developers-resource.landpress.line.me/fx/img/review_gold_star_28.png"
-            },
-            {
-              "type": "icon",
-              "size": "sm",
-              "url": "https://developers-resource.landpress.line.me/fx/img/review_gold_star_28.png"
-            },
-            {
-              "type": "icon",
-              "size": "sm",
-              "url": "https://developers-resource.landpress.line.me/fx/img/review_gray_star_28.png"
-            },
-            {
-              "type": "text",
-              "text": "4.0",
-              "size": "sm",
-              "color": "#999999",
-              "margin": "md",
-              "flex": 0
-            }
-          ]
-        },
-        {
-          "type": "box",
-          "layout": "vertical",
-          "margin": "lg",
-          "spacing": "sm",
-          "contents": [
-            {
-              "type": "box",
-              "layout": "baseline",
-              "spacing": "sm",
-              "contents": [
-                {
-                  "type": "text",
-                  "text": "Place",
-                  "color": "#aaaaaa",
-                  "size": "sm",
-                  "flex": 1
-                },
-                {
-                  "type": "text",
-                  "text": "Flex Tower, 7-7-4 Midori-ku, Tokyo",
-                  "wrap": true,
-                  "color": "#666666",
-                  "size": "sm",
-                  "flex": 5
-                }
-              ]
-            },
-            {
-              "type": "box",
-              "layout": "baseline",
-              "spacing": "sm",
-              "contents": [
-                {
-                  "type": "text",
-                  "text": "Time",
-                  "color": "#aaaaaa",
-                  "size": "sm",
-                  "flex": 1
-                },
-                {
-                  "type": "text",
-                  "text": "10:00 - 23:00",
-                  "wrap": true,
-                  "color": "#666666",
-                  "size": "sm",
-                  "flex": 5
-                }
-              ]
-            }
-          ]
-        }
-      ]
-    },
-    "footer": {
-      "type": "box",
-      "layout": "vertical",
-      "spacing": "sm",
-      "contents": [
-        {
-          "type": "button",
-          "style": "link",
-          "height": "sm",
-          "action": {
-            "type": "uri",
-            "label": "CALL",
-            "uri": "https://line.me/"
-          }
-        },
-        {
-          "type": "button",
-          "style": "link",
-          "height": "sm",
-          "action": {
-            "type": "uri",
-            "label": "WEBSITE",
-            "uri": "https://line.me/"
-          }
-        },
-        {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [],
-          "margin": "sm"
-        }
-      ],
-      "flex": 0
-    }
+    ]
   }
   event.reply(msg)
 })
